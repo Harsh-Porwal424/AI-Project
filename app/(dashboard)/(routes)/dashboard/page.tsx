3b1aa4625a55ca167d6ffa74be7e941c4c8f5c62
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2023-09-23 01:06:04 +0530

    Created SideBar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button"
+
 
 export default function DashboardPage() {
   return (
 
     <div>
         <p className="text-6xl text-green-500"> DashBoard Page (Protected)</p>
-        <Button variant="destructive">Button</Button>
-        <UserButton afterSignOutUrl="/"/>
+
     </div>
 
   )
